ab90dda7e34bac8cd7a802c56cf8d8f01dc82f5d
William S <w.s@w.s>
2020-01-03 08:41:37 -0500

    nonoverlapping screen sync
    new connections extend the image to the right
    mobile device dragging and sync

--- serv.py
+++ serv.py
@@ -7,25 +7,32 @@ import json
 import logging
 import websockets
 
-logging.basicConfig()
+#logging.basicConfig()
+import sys
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
-STATE = {"x": 0, "y": 0}
+POSITION = {"x": 0, "y": 0}
 
-USERS = set()
+USERS = set() # aka connected
 
+windowSizes = {} # key: websocket, value: (l,w)
+offsets = {} # key: websocket, value: x,y. values are 0 or negative
 
-def state_event():
-	return json.dumps({"type": "state", **STATE})
+def pos_event(ws):
+	return json.dumps(
+		{"type": "pos",
+		'x': POSITION['x'] + offsets[ws][0],
+		'y': POSITION['y'] + offsets[ws][1]}
+	)
 
 
 #def users_event():
 #	return json.dumps({"type": "users", "count": len(USERS)})
 
 
-async def notify_state():
+async def notify_pos():
 	if USERS:  # asyncio.wait doesn't accept an empty list
-		message = state_event()
-		await asyncio.wait([user.send(message) for user in USERS])
+		await asyncio.wait([user.send(pos_event(user)) for user in USERS])
 
 
 #async def notify_users():
@@ -36,24 +43,59 @@ async def notify_state():
 
 async def register(websocket):
 	USERS.add(websocket)
+	# logic for how to arrange the displays
+	if len(offsets) == 0:
+		offsets[websocket] = [0,0]
+	else: #right now just putting things to the right of it
+		x = y = 0
+		for k,v in windowSizes.items(): # TODO: i think should be max(offset)+windowSize[thing], but maybe not because this isn't causing problems
+			x -= v[0]
+			#y += v[1]
+		offsets[websocket] = [x,y]
+
+	print("Offsets: ", offsets)
 #	await notify_users()
+#	logging.info("ws connected")
 
 
 async def unregister(websocket):
 	USERS.remove(websocket)
-#	await notify_users()
+	w,h = windowSizes.pop(websocket, (0,0))
 
+	# rearrange displays
+	x,y = offsets.pop(websocket, (0,0))
+	for user in USERS: # affected offsets are those to the right aka more negative
+		if offsets[user][0] < x:
+			offsets[user][0] += w
+
+	#print("Offsets: ", offsets)
+	if not USERS:
+		POSITION['x'] = POSITION['y'] = 0 # TODO: if something long like pdf only x goes to zero but y stays the same
+	await notify_pos()
 
 async def position_updater(websocket, path):
 	# register(websocket) sends user_event() to websocket
 	await register(websocket)
 	try:
-		await websocket.send(state_event())
+		#await websocket.send(pos_event(websocket)) # don't need this anymore because JS sends onconnect
 		async for message in websocket:
 			data = json.loads(message)
-			STATE['x'] = data['x']
-			STATE['y'] = data['y']
-			await notify_state()
+			if data['type'] == 'pos':
+				POSITION['x'] = data['x'] - offsets[websocket][0]
+				POSITION['y'] = data['y'] - offsets[websocket][1] # currently offset y is zero for all
+			elif data['type'] == 'dim':
+#				if websocket in windowSizes:
+#					delta_x = data['w'] - windowSizes[websocket][0]
+#					#delta_y = data['h'] - windowSizes[websocket][1]
+#					for user in USERS: # TODO: fix this. subsequent windows are not going as fast as the window is resizing, causing overlap
+#						if offsets[user][0] < windowSizes[websocket][0]:
+#							offsets[user][0] -= delta_x
+				windowSizes[websocket] = data['w'], data['h']
+				logging.info("window size: %sx%s",  data['w'], data['h'])
+
+			else:
+				logging.error("unsupported event: {}", data)
+			await notify_pos()
 	finally:
 		await unregister(websocket)
 
