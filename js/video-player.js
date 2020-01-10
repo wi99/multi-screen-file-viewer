cd95cdb3a1dc4d49c8fd79d917d8278dbe17a6d0
William S <w.s@w.s>
2020-01-09 20:51:47 -0500

    video player
    play, pause, and jump is synced. volume control is local

---
+++ js/video-player.js
@@ -0,0 +1,133 @@
+(function () {
+	'use strict';
+
+	// Does the browser actually support the video element?
+	var supportsVideo = !!document.createElement('video').canPlayType;
+
+	if (supportsVideo) {
+		// Obtain handles to main elements
+		var videoContainer = document.getElementById('container');
+		var video = document.getElementById('display');
+		var videoControls = document.getElementById('controls');
+
+		// Hide the default controls
+		video.controls = false;
+
+		// Display the user defined video controls
+		videoControls.setAttribute('data-state', 'visible');
+
+		// Obtain handles to buttons and other elements
+		var playpause = document.getElementById('playpause');
+		var mute = document.getElementById('mute');
+		var volinc = document.getElementById('volinc');
+		var voldec = document.getElementById('voldec');
+		var progress = document.getElementById('progress');
+		var progressBar = document.getElementById('progress-bar');
+
+		// If the browser doesn't support the progress element, set its state for some different styling
+		var supportsProgress = (document.createElement('progress').max !== undefined);
+		if (!supportsProgress) progress.setAttribute('data-state', 'fake');
+
+		// Check the volume
+		var checkVolume = function(dir) {
+			if (dir) {
+				var currentVolume = Math.floor(video.volume * 10) / 10;
+				if (dir === '+') {
+					if (currentVolume < 1) video.volume += 0.1;
+				}
+				else if (dir === '-') {
+					if (currentVolume > 0) video.volume -= 0.1;
+				}
+				// If the volume has been turned off, also set it as muted
+				// Note: can only do this with the custom control set as when the 'volumechange' event is raised, there is no way to know if it was via a volume or a mute change
+				if (currentVolume <= 0) video.muted = true;
+				else video.muted = false;
+			}
+			changeButtonState('mute');
+		}
+
+		// Change the volume
+		var alterVolume = function(dir) {
+			checkVolume(dir);
+		}
+
+		if (document.addEventListener) {
+			// Wait for the video's meta data to be loaded, then set the progress bar's max value to the duration of the video
+			video.addEventListener('loadedmetadata', function() {
+				progress.setAttribute('max', video.duration);
+			});
+
+			var changeButtonState = function(type) {
+				// Play/Pause button
+				if (type == 'playpause') {
+					if (video.paused || video.ended) {
+						playpause.setAttribute('data-state', 'play');
+					}
+					else {
+						playpause.setAttribute('data-state', 'pause');
+					}
+				}
+				// Mute button
+				else if (type == 'mute') {
+					mute.setAttribute('data-state', video.muted ? 'unmute' : 'mute');
+				}
+			}
+
+			// Add event listeners for video specific events
+			video.addEventListener('play', function() {
+				changeButtonState('playpause');
+			}, false);
+			video.addEventListener('pause', function() {
+				changeButtonState('playpause');
+			}, false);
+			video.addEventListener('volumechange', function() {
+				checkVolume();
+			}, false);
+			video.addEventListener('ended', function() {
+				websocket.send(JSON.stringify({type: 'ctrl', action: 'pause', time: 0}));
+			}, false);
+
+			// Add events for all buttons			
+			playpause.addEventListener('click', function(e) {
+				if (video.paused || video.ended) {
+					var action = 'play'
+					//video.play();
+				}
+				else {
+					var action = 'pause'
+					//video.pause();
+				}
+				websocket.send(JSON.stringify({type: 'ctrl', action: action, time: video.currentTime}));
+			});			
+
+			mute.addEventListener('click', function(e) {
+				video.muted = !video.muted;
+				changeButtonState('mute');
+			});
+			volinc.addEventListener('click', function(e) {
+				alterVolume('+');
+			});
+			voldec.addEventListener('click', function(e) {
+				alterVolume('-');
+			});
+
+			// As the video is playing, update the progress bar
+			video.addEventListener('timeupdate', function() {
+				// For mobile browsers, ensure that the progress element's max attribute is set
+				if (!progress.getAttribute('max')) progress.setAttribute('max', video.duration);
+				progress.value = video.currentTime;
+				progressBar.style.width = Math.floor((video.currentTime / video.duration) * 100) + '%';
+			});
+
+			// React to the user clicking within the progress bar
+			progress.addEventListener('click', function(e) {
+				var pos = (e.pageX  - (this.offsetLeft + this.offsetParent.offsetLeft)) / this.offsetWidth;
+				//video.currentTime = pos * video.duration;
+				//websocket.send(JSON.stringify({type: 'ctrl', action: 'progress', time: video.currentTime}));
+				var action = video.paused ? 'pause' : 'play';
+				websocket.send(JSON.stringify({type: 'ctrl', action: action, time: pos * video.duration}));
+			});
+		}
+	 }
+
+ })();
--- serv.py
+++ serv.py
@@ -1,22 +1,32 @@
 
-# https://websockets.readthedocs.io/en/stable/intro.html#synchronization-example
-# WS server example that synchronizes state across clients
 
 import asyncio
 import json
 import logging
 import websockets
+import time
 
-#logging.basicConfig()
-import sys
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.basicConfig()
+#import sys
+#logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 POSITION = {"x": 0, "y": 0}
 
-USERS = set() # aka connected
+SCREENS = set() # aka connected
 
 windowSizes = {} # key: websocket, value: (l,w)
 offsets = {} # key: websocket, value: x,y. values are 0 or negative
+
+totalWidth = 0
+totalHeight = 0
+maxWidth = 0
+maxHeight = 0
+
+# TODO: make these persist after exiting program.
+class VideoState:
+	saveTime = time.time()
+	playbackTime = 0
+	paused = True
 
 def pos_event(ws):
 	return json.dumps(
@@ -25,77 +35,119 @@ def pos_event(ws):
 		'y': POSITION['y'] + offsets[ws][1]}
 	)
 
+def dim_event():
+	## Fit Width
+	#return json.dumps({'type': 'dim', 'w': str(totalWidth) + 'px', 'h': 'auto'})
+	## Fit Height
+	#return json.dumps({'type': 'dim', 'w': 'auto', 'h': str(totalHeight) + 'px'})
+	## Fit Page
+	return json.dumps({'type': 'dim', 'w': str(totalWidth) + 'px', 'h': str(maxHeight) + 'px'})
 
-#def users_event():
-#	return json.dumps({"type": "users", "count": len(USERS)})
+def action_event(action):
+	return json.dumps({'type': 'ctrl', 'action': action})
 
 
 async def notify_pos():
-	if USERS:  # asyncio.wait doesn't accept an empty list
-		await asyncio.wait([user.send(pos_event(user)) for user in USERS])
+	if SCREENS:  # asyncio.wait doesn't accept an empty list
+		await asyncio.wait([screen.send(pos_event(screen)) for screen in SCREENS])
 
+async def notify_dim():
+	if SCREENS:
+		message = dim_event()
+		await asyncio.wait([screen.send(message) for screen in SCREENS])
 
-#async def notify_users():
-#	if USERS:  # asyncio.wait doesn't accept an empty list
-#		message = users_event()
-#		await asyncio.wait([user.send(message) for user in USERS])
+async def notify_relay(data):
+	if SCREENS:
+		message = json.dumps(data)
+		await asyncio.wait([screen.send(message) for screen in SCREENS])
 
 
 async def register(websocket):
-	USERS.add(websocket)
+	SCREENS.add(websocket)
 	# logic for how to arrange the displays
-	if len(offsets) == 0:
+	if not offsets:
 		offsets[websocket] = [0,0]
-	else: #right now just putting things to the right of it
-		x = y = 0
-		for k,v in windowSizes.items(): # TODO: i think should be max(offset)+windowSize[thing], but maybe not because this isn't causing problems
-			x -= v[0]
-			#y += v[1]
-		offsets[websocket] = [x,y]
+	else: #right now just extending displays to the right
+		global totalWidth
+		offsets[websocket] = [-totalWidth, 0]
 
-	print("Offsets: ", offsets)
-#	await notify_users()
-#	logging.info("ws connected")
+	windowSizes[websocket] = (0,0)
+	
+	if not VideoState.paused:
+		VideoState.playbackTime += time.time() - VideoState.saveTime
+	action = 'pause' if VideoState.paused else 'play'
+	await websocket.send(json.dumps({"type": "ctrl", "action": action, "time": VideoState.playbackTime}))
 
 
 async def unregister(websocket):
-	USERS.remove(websocket)
+	SCREENS.remove(websocket)
 	w,h = windowSizes.pop(websocket, (0,0))
+
+	global totalWidth, totalHeight
+	totalWidth -= w
+	totalHeight -= h
 
 	# rearrange displays
 	x,y = offsets.pop(websocket, (0,0))
-	for user in USERS: # affected offsets are those to the right aka more negative
-		if offsets[user][0] < x:
-			offsets[user][0] += w
+	for screen in SCREENS: # affected offsets are those to the right aka more negative
+		if offsets[screen][0] < x:
+			offsets[screen][0] += w
 
-	#print("Offsets: ", offsets)
-	if not USERS:
+	# reset and revert values
+	if not SCREENS:
+		# general
 		POSITION['x'] = POSITION['y'] = 0 # TODO: if something long like pdf only x goes to zero but y stays the same
+	else:
+		global maxWidth, maxHeight
+		maxWidth = max(windowSizes.values())[0]
+		maxHeight = max(windowSizes.values(), key=lambda x:x[1])[1]
+	
+	# save values
+	## video
+	if not VideoState.paused:
+		VideoState.playbackTime += time.time() - VideoState.saveTime # will be inaccurate if the last client disconnects the wrong way.
 	await notify_pos()
+	await notify_dim()
 
 async def position_updater(websocket, path):
-	# register(websocket) sends user_event() to websocket
 	await register(websocket)
 	try:
-		#await websocket.send(pos_event(websocket)) # don't need this anymore because JS sends onconnect
 		async for message in websocket:
 			data = json.loads(message)
 			if data['type'] == 'pos':
 				POSITION['x'] = data['x'] - offsets[websocket][0]
-				POSITION['y'] = data['y'] - offsets[websocket][1] # currently offset y is zero for all
-			elif data['type'] == 'dim':
-#				if websocket in windowSizes:
-#					delta_x = data['w'] - windowSizes[websocket][0]
-#					#delta_y = data['h'] - windowSizes[websocket][1]
-#					for user in USERS: # TODO: fix this. subsequent windows are not going as fast as the window is resizing, causing overlap
-#						if offsets[user][0] < windowSizes[websocket][0]:
-#							offsets[user][0] -= delta_x
-				windowSizes[websocket] = data['w'], data['h']
-				logging.info("window size: %sx%s",  data['w'], data['h'])
+				POSITION['y'] = data['y'] - offsets[websocket][1]
+				await notify_pos()
 
+			elif data['type'] == 'dim':
+				global totalWidth, totalHeight, maxWidth, maxHeight
+				delta_x = data['w'] - windowSizes[websocket][0]
+				delta_y = data['h'] - windowSizes[websocket][1]
+				totalWidth += delta_x
+				totalHeight += delta_y
+				if maxWidth < data['w']:
+					maxWidth = data['w']
+				if maxHeight < data['h']:
+					maxHeight = data['h']
+				for screen in SCREENS:
+					if offsets[screen][0] < offsets[websocket][0]:
+						offsets[screen][0] -= delta_x
+				windowSizes[websocket] = (data['w'], data['h'])
+				logging.info("window size: %sx%s",  data['w'], data['h'])
+				await notify_dim()
+				await notify_pos()
+
+			elif data['type'] == 'ctrl':
+				await notify_relay(data)
+				VideoState.playbackTime = data['time']
+				VideoState.saveTime = time.time()
+				if data['action'] == 'pause':
+					VideoState.paused = True
+				else: # data['action'] == 'play'
+					VideoState.paused = False
 			else:
 				logging.error("unsupported event: {}", data)
-			await notify_pos()
+
 	finally:
 		await unregister(websocket)
 
